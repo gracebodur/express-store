55362995c0f1a8be3edf6da44fe4646678a18280
Grace <mgracebodur@gmail.com>
2019-12-23 23:58:03 -0500

    Validation

--- src/app.js
+++ src/app.js
@@ -17,13 +17,74 @@ app.use(cors())
 app.use(helmet())
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('A GET Request')
-})
+// app.get('/', (req, res) => {
+//     res.send('A GET Request')
+// })
 
-app.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('POST request received')
+// app.post('/', (req, res) => {
+//     console.log(req.body)
+//     res.send('POST request received')
+// })
+
+app.post('/user', (req, res) => {
+    //get the data
+    const { username, password, favoriteClub, newsLetter = false } = req.body
+    //validation code
+    // All are required, check if they were sent
+    if (!username) {
+        return res
+            .status(400)
+            .send('Username required')
+    }
+
+    if(!password) {
+        return res
+            .status(400)
+            .send('Password required')
+    }
+
+    if (!favoriteClub) {
+        return res
+            .status(400)
+            .send('favorite Club required')
+    }
+
+    // make sure username is correctly formatted
+    if(username.length < 6 || username.length > 20 ) {
+        return res
+            .status(400)
+            .send('Username must be between 6 and 20 characters')
+    }
+
+    //password length
+    if(password.length < 8 || password.length > 36) {
+        return res
+            .send(400)
+            .send('Password must be between 8 and 36 characters')
+    }
+
+    //password contains digit, using a regex here
+    if(!password.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)) {
+        return res
+            .status(400)
+            .send('Password must contain at least one digit')
+    }
+
+    const clubs = [
+        'Cache Valley Stone Society',
+        'Ogden Curling Club',
+        'Park City Curling Club',
+        'Salt City Curling Club',
+        'Utah Olympic Oval Curling Club'
+    ]
+
+    //make sure the club is valid
+    if (!clubs.includes(favoriteClub)) {
+        return res
+            .status(400)
+            .send('Not a valid Club')
+    }
+    res.send('All Validation passed')
 })
 
 app.use(function errorHandler(error, req, res, next) {
